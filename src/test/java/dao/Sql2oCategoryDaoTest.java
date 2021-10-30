5149e141a0fb98f5d10196040c6a999738adcd5d
lokified <sheldon.obonyo@student.moringaschool.com>
2021-10-30 14:56:59 +0300

    test app

---
+++ src/test/java/dao/Sql2oCategoryDaoTest.java
@@ -0,0 +1,118 @@
+package dao;
+
+import models.Category;
+import models.Task;
+import org.junit.*;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Sql2oCategoryDaoTest {
+    private static Sql2oCategoryDao categoryDao;
+    private static Sql2oTaskDao taskDao;
+    private static Connection conn;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/todolist_test";
+        Sql2o sql2o = new Sql2o(connectionString, "moringa","dammey5");
+        categoryDao = new Sql2oCategoryDao(sql2o);
+        taskDao = new Sql2oTaskDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @After // run after every test
+    public void tearDown() throws Exception { //I have changed
+        System.out.println("clearing database");
+        categoryDao.clearAllCategories(); // clear all categories after every test
+        taskDao.clearAllTasks(); // clear all tasks after every test
+    }
+
+    @AfterClass // changed to @AfterClass (run once after all tests in this file completed)
+    public static void shutDown() throws Exception { //changed to static and shutDown
+        conn.close(); // close connection once after this entire test file is finished
+        System.out.println("connection closed");
+    }
+
+    @Test
+    public void addingCategorySetsId() throws Exception {
+        Category category = setupNewCategory();
+        int originalCategoryId = category.getId();
+        categoryDao.add(category);
+        assertNotEquals(originalCategoryId, category.getId());
+    }
+
+    @Test
+    public void existingCategoriesCanBeFoundById() throws Exception {
+        Category category = setupNewCategory();
+        categoryDao.add(category);
+        Category foundCategory = categoryDao.findById(category.getId());
+        assertEquals(category, foundCategory);
+    }
+
+    @Test
+    public void addedCategoriesAreReturnedFromGetAll() throws Exception {
+        Category category = setupNewCategory();
+        categoryDao.add(category);
+        assertEquals(1, categoryDao.getAll().size());
+    }
+
+    @Test
+    public void noCategoriesReturnsEmptyList() throws Exception {
+        assertEquals(0, categoryDao.getAll().size());
+    }
+
+    @Test
+    public void updateChangesCategoryContent() throws Exception {
+        String initialDescription = "Yardwork";
+        Category category = new Category (initialDescription);
+        categoryDao.add(category);
+        categoryDao.update(category.getId(),"Cleaning");
+        Category updatedCategory = categoryDao.findById(category.getId());
+        assertNotEquals(initialDescription, updatedCategory.getName());
+    }
+
+    @Test
+    public void deleteByIdDeletesCorrectCategory() throws Exception {
+        Category category = setupNewCategory();
+        categoryDao.add(category);
+        categoryDao.deleteById(category.getId());
+        assertEquals(0, categoryDao.getAll().size());
+    }
+
+    @Test
+    public void clearAllClearsAllCategories() throws Exception {
+        Category category = setupNewCategory();
+        Category otherCategory = new Category("Cleaning");
+        categoryDao.add(category);
+        categoryDao.add(otherCategory);
+        int daoSize = categoryDao.getAll().size();
+        categoryDao.clearAllCategories();
+        assertTrue(daoSize > 0 && daoSize > categoryDao.getAll().size());
+    }
+
+    @Test
+    public void getAllTasksByCategoryReturnsTasksCorrectly() throws Exception {
+        Category category = setupNewCategory();
+        categoryDao.add(category);
+        int categoryId = category.getId();
+        Task newTask = new Task("mow the lawn", categoryId);
+        Task otherTask = new Task("pull weeds", categoryId);
+        Task thirdTask = new Task("trim hedge", categoryId);
+        taskDao.add(newTask);
+        taskDao.add(otherTask); //we are not adding task 3 so we can test things precisely.
+        assertEquals(2, categoryDao.getAllTasksByCategory(categoryId).size());
+        assertTrue(categoryDao.getAllTasksByCategory(categoryId).contains(newTask));
+        assertTrue(categoryDao.getAllTasksByCategory(categoryId).contains(otherTask));
+        assertFalse(categoryDao.getAllTasksByCategory(categoryId).contains(thirdTask)); //things are accurate!
+    }
+
+    // helper method
+    public Category setupNewCategory(){
+        return new Category("Yardwork");
+    }
+}
---
+++ src/test/java/dao/Sql2oTaskDaoTest.java
@@ -0,0 +1,103 @@
+package dao;
+
+import models.Task;
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class Sql2oTaskDaoTest {
+    private static Sql2oTaskDao taskDao;                //these variables are now static.
+    private static Connection conn;                     //these variables are now static.
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        String connectionString = "jdbc:postgresql://localhost:5432/todolist_test"; // connect to postgres test database
+        Sql2o sql2o = new Sql2o(connectionString, "moringa", "dammey5");         // changed user and pass to null
+        taskDao = new Sql2oTaskDao(sql2o);
+        conn = sql2o.open();                                                        // open connection once before this test file is run
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("clearing database");
+        taskDao.clearAllTasks();
+
+    }
+
+    @AfterClass
+    public static void shutDown() throws Exception{
+        conn.close();
+        System.out.println("connection closed");
+    }
+
+    @Test
+    public void addingTaskSetsId() throws Exception {
+        Task task = setupNewTask();
+        int originalTaskId = task.getId();
+        taskDao.add(task);
+        assertNotEquals(originalTaskId, task.getId()); //how does this work?
+    }
+
+    @Test
+    public void existingTasksCanBeFoundById() throws Exception {
+        Task task = setupNewTask();
+        taskDao.add(task); //add to dao (takes care of saving)
+        Task foundTask = taskDao.findById(task.getId()); //retrieve
+        assertEquals(task, foundTask); //should be the same
+    }
+
+    @Test
+    public void addedTasksAreReturnedFromgetAll() throws Exception {
+        Task task = setupNewTask();
+        taskDao.add(task);
+        assertEquals(1, taskDao.getAll().size());
+    }
+
+    @Test
+    public void noTasksReturnsEmptyList() throws Exception {
+        assertEquals(0, taskDao.getAll().size());
+    }
+
+    @Test
+    public void updateChangesTaskContent() throws Exception {
+        String initialDescription = "mow the lawn";
+        Task task = setupNewTask();
+        taskDao.add(task);
+
+        taskDao.update(task.getId(),"brush the cat", 1);
+        Task updatedTask = taskDao.findById(task.getId()); //why do I need to refind this?
+        assertNotEquals(initialDescription, updatedTask.getDescription());
+    }
+
+    @Test
+    public void deleteByIdDeletesCorrectTask() throws Exception {
+        Task task = setupNewTask();
+        taskDao.add(task);
+        taskDao.deleteById(task.getId());
+        assertEquals(0, taskDao.getAll().size());
+    }
+
+    @Test
+    public void clearAllClearsAll() throws Exception {
+        Task task = setupNewTask();
+        Task otherTask = new Task("brush the cat", 2);
+        taskDao.add(task);
+        taskDao.add(otherTask);
+        int daoSize = taskDao.getAll().size();
+        taskDao.clearAllTasks();
+        assertTrue(daoSize > 0 && daoSize > taskDao.getAll().size()); //this is a little overcomplicated, but illustrates well how we might use `assertTrue` in a different way.
+    }
+
+    @Test
+    public void categoryIdIsReturnedCorrectly() throws Exception {
+        Task task = setupNewTask();
+        int originalCatId = task.getCategoryId();
+        taskDao.add(task);
+        assertEquals(originalCatId, taskDao.findById(task.getId()).getCategoryId());
+    }
+
+    //define the following once and then call it as above in your tests.
+    public Task setupNewTask(){
+        return new Task("Mow the lawn", 1);
+    }
+}
